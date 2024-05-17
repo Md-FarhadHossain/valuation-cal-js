3cbca2af2f61481993dc8ff0d65c66312a9fce44
Md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2024-05-18 02:24:52 +0600

    just zero added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const cash = document.getElementById("cash")
 const percentage = document.getElementById("percentage")
 const valuation = document.getElementById("valuation")
+const cashType = document.getElementById("cashType")
 const calculateBtn = document.getElementById("calculateBtn")
 
 
@@ -8,12 +9,29 @@ const calculateBtn = document.getElementById("calculateBtn")
 
 calculateBtn.addEventListener("click", (e) => {
     e.preventDefault()
-    const cashValue = parseFloat(cash.value)
+    
+
+    if(cashType.value === "lakh") {
+        const lakh = cash.value + "00000"
+        const cashValue = parseFloat(lakh)
+        console.log(cashValue)
+        return cashValue
+    }
+    if(cashType.value === "crore") {
+        const crore = cash.value + "0000000"
+        const cashValue = parseFloat(crore)
+        console.log(cashValue)
+        return cashValue
+    }
+
+    console.log(cashValue)
     const percentageValue = parseFloat(percentage.value)
 
-    const divide = cashValue / percentageValue
 
-    console.log(divide * 100)
+    const divide = cashValue / percentageValue;
+    
+    valuation.innerHTML = divide
+    // console.log(divide * 100)
 
 
 })
